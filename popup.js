28252c2ffd687464b54573127d3ed2ec0c4c124b
AnhNguyen <hung000anh@gmail.com>
2026-06-23 14:30:34 +0700

    feat: thêm nguồn xbanxia.cc và 69shumi.net

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@
  * Refactored to use background script for long-running tasks.
  */
 
-const SOURCES = [Source17k, Source22biqu, SourceUukanshu, SourceJjwxc, SourceQidian, SourceBiquge, Source52shuku, SourceFanqienovel, Source69shuba, SourceNovel543, SourceKakuyomu, SourceSyosetu, SourcePixiv, SourceIxdzs8, SourceBookQQ, SourceHetushu];
+const SOURCES = [Source17k, Source22biqu, SourceUukanshu, SourceJjwxc, SourceQidian, SourceBiquge, Source52shuku, SourceFanqienovel, Source69shuba, SourceNovel543, SourceKakuyomu, SourceSyosetu, SourcePixiv, SourceIxdzs8, SourceBookQQ, SourceHetushu, SourceXbanxia, Source69shumi];
 function getSource(url) {
   return SOURCES.find(s => s.pattern.test(url)) || null;
 }
